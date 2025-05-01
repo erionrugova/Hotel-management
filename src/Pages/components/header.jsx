22040997f5e4ceca7eb41c5858109d4f8a41ce96
almiravershevcii <almiravershevci@gmail.com>
2025-05-01 15:24:24 +0200

    footer and header

--- src/Pages/components/header.jsx
+++ src/Pages/components/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="bg-blue-900 text-white p-4">
+    <header className="bg-blue-900  text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">Hotel Paradise</h1>
         <nav className="space-x-4">
