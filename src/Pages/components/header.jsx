c682eb57f2fa3310500b45f6efa7663881a10c21
erionrugova <erionrugova0@gmail.com>
2025-05-01 15:48:51 +0200

    login fix

--- src/Pages/components/header.jsx
+++ src/Pages/components/header.jsx
@@ -6,12 +6,10 @@ function Header() {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">Hotel Paradise</h1>
         <nav className="space-x-4">
-          <a href="#" className="hover:underline">Home</a>
-          <a href="#" className="hover:underline">Rooms</a>
-
-          <a href="#login" className="hover:underline">Admin</a>
-
-          <a href="#" className="hover:underline">Contact</a>
+          <a href="/" className="hover:underline">Home</a>
+          <a href="/" className="hover:underline">Rooms</a>
+          <a href="/about" className="hover:underline">Contact</a>
+          <a href="/login" className="hover:underline">Log in</a>
         </nav>
       </div>
     </header>
--- src/home.jsx
+++ src/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import About from "./Pages/About"; 
+import Login from "./Pages/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
     </Router>
   );
