f9405c46e3c8bab8f7bacc21c38e1bd6521fd53c
almiravershevcii <almiravershevci@gmail.com>
2025-05-01 15:21:52 +0200

    footer and header

---
+++ src/Pages/components/header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-blue-900 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Hotel Paradise</h1>
+        <nav className="space-x-4">
+          <a href="#" className="hover:underline">Home</a>
+          <a href="#" className="hover:underline">Rooms</a>
+
+          <a href="#login" className="hover:underline">Admin</a>
+
+          <a href="#" className="hover:underline">Contact</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
