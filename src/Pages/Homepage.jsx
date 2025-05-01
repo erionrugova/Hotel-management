2b144cb820d9b1a88625712512ae4f68a9c9b28c
erionrugova <erionrugova0@gmail.com>
2025-05-01 14:51:27 +0200

    tailwind fix

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Homepage() {
     return (
         <div>
-            <h1>Hello world</h1>
+            <h1 className="text-3xl text-red-300">Hello world</h1>
         </div>
     )
 }
